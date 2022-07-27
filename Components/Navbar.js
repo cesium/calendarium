de38a4ba062fd24eaff6816ed1235530620ec707
Nuno Costa <75766357+Nuno23C@users.noreply.github.com>
2022-07-27 12:47:34 +0100

    Nc/style update (#16)
    * style update
    
    * style update

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,17 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 import Link from "next/link";
-import { navbarItems } from "./NavItems";
-import Dropdown from "./Dropdown";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 export default function Navbar() {
-    const [dropdown, setDropdown] = useState(false);
+    const exportPDF = () => {
+        const input = document.getElementById("APP");
+        html2canvas(input, {
+            logging: true,
+            letterRendering: 1,
+            userCors: true,
+        }).then((canvas) => {
+            const imgwidth = 208;
+            const imgHeight = (canvas.height * imgwidth) / canvas.width;
+            const imgData = canvas.toDataURL("img/png");
+            const pdf = new jsPDF("p", "mm", "a4");
+            pdf.addImage(imgData, "PNG", 0, 0, imgwidth, imgHeight);
+            pdf.save("calendario.pdf");
+        });
+    };
 
     return (
         <nav className="navbar">
             <Link href="https://cesium.link/">
                 <img src="./cesium-full-logo.png" className="nav-cesium-logo" />
             </Link>
-            <Dropdown />
-        </nav>   
+            <img src="./calendar-icon.ico" />
+            <button onClick={() => exportPDF()} className="navbar-button-pdf">
+                Extract to PDF
+            </button>
+        </nav>
     );
 }
