404b2a2fba9e33a4841f1bf942fd2009fa0165f1
Nuno23C <nunorocosta@gmail.com>
2022-07-27 11:01:17 +0100

    style update

--- pages/index.js
+++ pages/index.js
@@ -1,95 +1,70 @@
-import TextBox from './textBox'
-import Navbar from '../Components/Navbar'
-import emailjs from "emailjs-com"
-import Head from 'next/head'
-import Image from 'next/image'
-import { Calendar, momentLocalizer } from "react-big-calendar"
-import moment from "moment"
-import "moment/locale/en-gb"
-import styles from '../styles/Home.module.css'
-import "react-big-calendar/lib/css/react-big-calendar.css"
-import myEvents from './events'
-import  html2canvas from "html2canvas";
-import jsPDF from "jspdf";
+import TextBox from "./textBox";
+import Navbar from "../Components/Navbar";
+import Head from "next/head";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "moment/locale/en-gb";
+import styles from "../styles/Home.module.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import myEvents from "./events";
+import Layout from "antd/lib/layout/layout";
 
 //To localize the format of the calendar
 const localizer = momentLocalizer(moment);
 
-
 export default function Home() {
-    const exportPDF = () => {
-        const input = document.getElementById("APP")
-        html2canvas(input,{logging:true, letterRendering:1,userCors: true}).then(canvas=>{
-            const imgwidth= 208;
-            const imgHeight= canvas.height * imgwidth/ canvas.width;
-            const imgData = canvas.toDataURL('img/png')
-            const pdf = new jsPDF ('p','mm','a4');
-            pdf.addImage(imgData, 'PNG',0,0,imgwidth, imgHeight);
-            pdf.save("calendario.pdf")
-        })
-
-    }
-
-    return ( 
+    return (
         <div className="Home">
-            <button onClick={() => exportPDF()}>Tranferir para PDF</button>
+            <div className={styles.container}>
+                <Head>
+                    <title>Calendarium</title>
+                    <meta
+                        name="Calendarium"
+                        content="Calendar of events and exams"
+                    />
+                    <link rel="icon" href="/calendar-icon.ico" />~
+                </Head>
+                <Navbar />
 
-      
-      <div className={styles.container}>
-            <Head>
-                <title>Calendarium</title>
-                <meta
-                    name="Calendarium"
-                    content="Calendar of events and exams"
-                />
-                <link rel="icon" href="/calendar-icon.ico" />
-            </Head>
-            <Navbar />
+                <div id="APP" className={styles.calendar}>
+                    <Calendar
+                        localizer={localizer}
+                        //Establishing some default definitions
+                        defaultDate={new Date()}
+                        defaultView="month"
+                        //Custom event colors by their year (groupID)
+                        eventPropGetter={(event) => {
+                            var colors = [
+                                "#f07c54",
+                                "#f0c954",
+                                "#7b54f0",
+                                "#f0547b",
+                                "#5ac77b",
+                                "#5532a8",
+                            ];
 
-            <div id= "APP" className={styles.calendar}>
-                <Calendar
-                    localizer={localizer}
-                    //Establishing some default definitions
-                    defaultDate={new Date()}
-                    defaultView="month"
-                    //Custom event colors by their year (groupID)
-                    eventPropGetter={(event) => {
-                        var colors = [
-                            "#f07c54",
-                            "#f0c954",
-                            "#7b54f0",
-                            "#f0547b",
-                            "#5ac77b",
-                            "#5532a8",
-                        ];
+                            let newStyle = {
+                                backgroundColor: colors[event.groupId],
+                                border: "none",
+                            };
 
-                        let newStyle = {
-                            backgroundColor: colors[event.groupId],
-                            border: "none",
-                        };
-
-                        return { style: newStyle };
-                      }
-                    }
-                    
-                    //Using the array of all events
-                    events={myEvents}
-                    //Limit the time for the events (Between 8:00 and 20:00)
-                    min={new Date(2022, 0, 1, 8, 0)}
-                    max={new Date(2022, 0, 1, 21, 0)}
-                    style={{ height: "90vh" }}
-                    
-                />
-            
-           
+                            return { style: newStyle };
+                        }}
+                        //Using the array of all events
+                        events={myEvents}
+                        //Limit the time for the events (Between 8:00 and 20:00)
+                        min={new Date(2022, 0, 1, 8, 0)}
+                        max={new Date(2022, 0, 1, 21, 0)}
+                        style={{ height: "90vh" }}
+                    />
+                </div>
             </div>
-      <div className="textbox"> 
-          <TextBox />
-      </div>
-      <div className={styles.footer}>
-        <img src="/cesium-full-logo.png" width="15%"/>
-      </div>
-      
-    </div> 
-  )
+            <div className="textbox">
+                <TextBox />
+            </div>
+            <div className={styles.footer}>
+                <img src="/cesium-full-logo.png" width="15%" />
+            </div>
+        </div>
+    );
 }
--- pages/textBox.js
+++ pages/textBox.js
@@ -1,51 +1,46 @@
 import emailjs from "emailjs-com";
 
-
 const TextBox = () => {
-  function sendEmail(e) {
-    e.preventDefault();
+    function sendEmail(e) {
+        e.preventDefault();
 
-    emailjs.sendForm(
-      "service_oxvmxpc","template_cqb2zyg",
-      e.target, "53zgb406krqnsJIF_"
-    ).then(res=>{
-        console.log(res);
-    }).catch(err=> console.log(err));
-  }
-  return (
-    <div
-      className="container border"
-      style={{
-        marginTop: "30px",
-        width: "30%",
-        backgroundImage: `url('https://cdn.shopk.it/usercontent/1-04-store/media/images/square/d51d168-ceisum.png')`,
+        emailjs
+            .sendForm(
+                "service_oxvmxpc",
+                "template_cqb2zyg",
+                e.target,
+                "53zgb406krqnsJIF_"
+            )
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => console.log(err));
+    }
 
-      }}
-    >
-      <h1 style={{ marginTop: "25px", fontWeight: "bold" }}>Box for suggestions!</h1>
-      <form
-        className="row"
-        style={{ margin: "25px 85px 75px 100px",  backgroundColor: "#D3D3D3", padding:"10px 10px", border:"solid black", borderRadius: "5px"}}
-        onSubmit={sendEmail}
-      >
-        <label>Name</label>
-        <input type="text" name="name" className="form-control" style={{width: "200px"}} />
+    return (
+        <div className="txt-container-border">
+            <h1 style={{ marginTop: "25px", fontWeight: "bold" }}>
+                Box for suggestions!
+            </h1>
+            <form className="txt-caixa" onSubmit={sendEmail}>
+                <label>Name</label>
+                <input type="text" name="name" className="form-control" />
 
-        
-        <label>Email</label>
-        <input type="email" name="user_email" className="form-control" style={{width: "200px"}}  />
+                <label>Email</label>
+                <input
+                    type="email"
+                    name="user_email"
+                    className="form-control"
+                />
 
-        <label>Message</label>
-        <textarea name="message" rows="4" className="form-control" style={{width: "200px"}} />
-        <input
-          type="submit"
-          value="Send"
-          className="form-control btn btn-primary"
-          style={{ marginTop: "30px", border: "solid black", borderRadius: "5px", padding: "2px 5px", cursor: "pointer", backgroundColor: "#A9A9A9"}}
-        />
-      </form>
-    </div>
-  );
+                <label className="txt-msg">Message</label>
+
+                <textarea name="message" rows="4" className="form-control" />
+
+                <input type="submit" value="Send" className="txt-send-button" />
+            </form>
+        </div>
+    );
 };
 
 export default TextBox;
