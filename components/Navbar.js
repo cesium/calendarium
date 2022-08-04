abc30da0b079659717d21d961c69d9b46730c15c
Nelson Estevão <nelsonmestevao@proton.me>
2022-08-04 02:15:59 +0100

    Add formatter

---
+++ components/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+
+export default function Navbar() {
+  const exportPDF = () => {
+    const input = document.getElementById("APP");
+    html2canvas(input, {
+      logging: true,
+      letterRendering: 1,
+      userCors: true,
+    }).then((canvas) => {
+      const imgwidth = 208;
+      const imgHeight = (canvas.height * imgwidth) / canvas.width;
+      const imgData = canvas.toDataURL("img/png");
+      const pdf = new jsPDF("p", "mm", "a4");
+      pdf.addImage(imgData, "PNG", 0, 0, imgwidth, imgHeight);
+      pdf.save("calendario.pdf");
+    });
+  };
+
+  return (
+    <nav className="navbar">
+      <Link href="https://cesium.link/">
+        <img src="./cesium-full-logo.png" className="nav-cesium-logo" />
+      </Link>
+      <img src="./calendar-icon.ico" />
+      <button onClick={() => exportPDF()} className="navbar-button-pdf">
+        Extract to PDF
+      </button>
+    </nav>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,109 +1,100 @@
-import React, {useState} from "react";
-import TextBox from "../Components/TextBox";
-import Navbar from "../Components/Navbar";
+import React, { useState } from "react";
+import TextBox from "../components/TextBox";
+import Navbar from "../components/Navbar";
 import Head from "next/head";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "moment/locale/en-gb";
 import styles from "../styles/Home.module.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import myEvents from "../Components/Events";
-import CheckBox from "../Components/CheckBox";
+import myEvents from "../components/Events";
+import CheckBox from "../components/CheckBox";
 
 //To localize the format of the calendar
 const localizer = momentLocalizer(moment);
 
 export default function Home() {
+  //States and update functions for both Filters and Events
+  const [Events, setEvents] = useState(myEvents);
+  const [Filters, setFilters] = useState([]);
 
-    //States and update functions for both Filters and Events
-    const[Events, setEvents] = useState(myEvents)
-    const[Filters, setFilters] = useState([])
+  //Function to update the Events with the selected Filters
+  const showNewEvents = (f) => {
+    console.log(f);
+    console.log(myEvents);
 
-    //Function to update the Events with the selected Filters
-    const showNewEvents = (f) => {
+    const filters = Object.values(f);
+    const newEvents = [...myEvents];
 
-        console.log(f)
-        console.log(myEvents)
-
-        const filters = Object.values(f)
-        const newEvents = [...myEvents]
-
-        if (filters.length > 0){
-            newEvents = newEvents.filter((ev) => 
-                filters.includes(ev.filterId))
-        }
-
-        setEvents(newEvents)
-    }
-    
-    //Function to update the Filters state
-    const handleFilters = (myFilters) =>{
-        console.log(myFilters)
-        const newFilters = {...myFilters}
-
-        setFilters(newFilters)
-        showNewEvents(newFilters)
+    if (filters.length > 0) {
+      newEvents = newEvents.filter((ev) => filters.includes(ev.filterId));
     }
 
-    return (
-        <div className="Home">
-            <div className={styles.container}>
-                <Head>
-                    <title>Calendarium</title>
-                    <meta
-                        name="Calendarium"
-                        content="Calendar of events and exams"
-                    />
-                    <link rel="icon" href="/calendar-icon.ico" />~
-                </Head>
-                <Navbar />
+    setEvents(newEvents);
+  };
 
-                <div id="APP" className={styles.calendar}>
-                    <Calendar
-                        localizer={localizer}
-                        //Establishing some default definitions
-                        defaultDate={new Date()}
-                        defaultView="month"
-                        //Custom event colors by their year (groupID)
-                        eventPropGetter={(event) => {
-                            var colors = [
-                                "#f07c54",
-                                "#f0c954",
-                                "#7b54f0",
-                                "#f0547b",
-                                "#5ac77b",
-                                "#5532a8",
-                                "#b70a0a"
-                            ];
+  //Function to update the Filters state
+  const handleFilters = (myFilters) => {
+    console.log(myFilters);
+    const newFilters = { ...myFilters };
 
-                            let newStyle = {
-                                backgroundColor: colors[event.groupId],
-                                border: "none",
-                            };
+    setFilters(newFilters);
+    showNewEvents(newFilters);
+  };
 
-                            return { style: newStyle };
-                        }}
-                        //Using the array of all events
-                        events={Events}
-                        //Limit the time for the events (Between 8:00 and 20:00)
-                        min={new Date(2022, 0, 1, 8, 0)}
-                        max={new Date(2022, 0, 1, 21, 0)}
-                        style={{ height: "90vh" }}
-                    />
-                </div>
-                <div className={styles.filter}>
-                    <CheckBox 
-                        handleFilters={myFilters => handleFilters(myFilters)}
-                    />
-                </div>
+  return (
+    <div className="Home">
+      <div className={styles.container}>
+        <Head>
+          <title>Calendarium</title>
+          <meta name="Calendarium" content="Calendar of events and exams" />
+          <link rel="icon" href="/calendar-icon.ico" />~
+        </Head>
+        <Navbar />
 
-            </div>
-            <div className="textbox">
-                <TextBox />
-            </div>
-            <div className={styles.footer}>
-                <img src="/cesium-full-logo.png" width="15%" />
-            </div>
+        <div id="APP" className={styles.calendar}>
+          <Calendar
+            localizer={localizer}
+            //Establishing some default definitions
+            defaultDate={new Date()}
+            defaultView="month"
+            //Custom event colors by their year (groupID)
+            eventPropGetter={(event) => {
+              var colors = [
+                "#f07c54",
+                "#f0c954",
+                "#7b54f0",
+                "#f0547b",
+                "#5ac77b",
+                "#5532a8",
+                "#b70a0a",
+              ];
+
+              let newStyle = {
+                backgroundColor: colors[event.groupId],
+                border: "none",
+              };
+
+              return { style: newStyle };
+            }}
+            //Using the array of all events
+            events={Events}
+            //Limit the time for the events (Between 8:00 and 20:00)
+            min={new Date(2022, 0, 1, 8, 0)}
+            max={new Date(2022, 0, 1, 21, 0)}
+            style={{ height: "90vh" }}
+          />
         </div>
-    );
+        <div className={styles.filter}>
+          <CheckBox handleFilters={(myFilters) => handleFilters(myFilters)} />
+        </div>
+      </div>
+      <div className="textbox">
+        <TextBox />
+      </div>
+      <div className={styles.footer}>
+        <img src="/cesium-full-logo.png" width="15%" />
+      </div>
+    </div>
+  );
 }
