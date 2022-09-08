69c4886c42a2f7dfb63af11a070a55095ccad3e5
Mário Rodrigues <93675410+MarioRodrigues10@users.noreply.github.com>
2022-09-08 22:24:29 +0100

    Add Dark Mode Button  (#25)
    * Add dark mode feature
    
    * run format
    
    * Improve design
    
    * Add local storage
    
    * Save theme in local storage
    
    * Match body theme with local storage

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,8 +2,15 @@ import Link from "next/link";
 import Image from "next/image";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
+import { useTheme } from "./Theme/Theme";
+import styles from "./style.module.css";
 
 export default function Navbar() {
+  const { isDark, toggleTheme } = useTheme();
+  if (isDark) {
+    document.body.classList.add("dark");
+  }
+
   const exportPDF = () => {
     const input = document.getElementById("APP");
     html2canvas(input, {
@@ -20,26 +27,95 @@ export default function Navbar() {
     });
   };
 
+  const darkMode = () => {
+    const body = document.querySelector("body");
+    body.classList.toggle("dark");
+  };
+
+  function DarkModeToggle({ visible }) {
+    return (
+      visible && (
+        <button className="btn" onClick={toggleTheme}>
+          {isDark ? (
+            <div className={styles.dark}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                role="img"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                ></path>
+              </svg>
+            </div>
+          ) : (
+            <div className={styles.light}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                role="img"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                ></path>
+              </svg>
+            </div>
+          )}
+        </button>
+      )
+    );
+  }
+
   return (
     <nav className="navbar">
-      <Link href="https://cesium.link/">
-        <Image
-          width={100}
-          height={21}
-          src="/cesium-full-logo.png"
-          className="nav-cesium-logo"
-          alt="CeSIUM Link"
-        />
-      </Link>
+      {isDark ? (
+        <Link href="https://cesium.link/">
+          <Image
+            width={100}
+            height={21}
+            src="/cesium-LIGHT.svg"
+            className="nav-cesium-logo"
+            alt="CeSIUM Link"
+          />
+        </Link>
+      ) : (
+        <Link href="https://cesium.link/">
+          <Image
+            width={100}
+            height={21}
+            src="/cesium-full-logo.png"
+            className="nav-cesium-logo"
+            alt="CeSIUM Link"
+          />
+        </Link>
+      )}
+
       <Image
         width={32}
         height={21}
         src="/calendar-icon.ico"
         alt="Calendarium"
       />
-      <button onClick={() => exportPDF()} className="navbar-button-pdf">
-        Extract to PDF
-      </button>
+      <div className="navbar-buttons">
+        <button onClick={() => exportPDF()} className="navbar-button-pdf">
+          Extract to PDF
+        </button>
+        <button onClick={() => darkMode()} className="navbar-button-dark">
+          <DarkModeToggle visible={true} />
+        </button>
+      </div>
     </nav>
   );
 }
