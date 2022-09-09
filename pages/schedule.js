a87adb0e009098e2fb8e912b5098070dd7f36e12
Enzo Vieira <enzovieira16@hotmail.com>
2022-09-09 22:52:50 +0100

    add(schedule.js): List filtered schedule
    The User can filter his schedule based on his subjects

--- pages/schedule.js
+++ pages/schedule.js
@@ -1,54 +1,67 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import Head from "next/head";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
+import path from "path";
+import fsPromises from "fs/promises";
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 import { Layout } from "../components/Layout";
+import CheckBox from "../components/CheckBox";
 
 const localizer = momentLocalizer(moment);
 
-const colors = {
-  Análise: "#b70a0a",
-  Lógica: "#f0c954",
-};
+const mock = [
+  {
+    id: 12345,
+    title: "Análise",
+    theoretical: true,
+    shift: "TP2",
+    building: "7",
+    room: "0.02",
+    day: 1,
+    start: "09:00",
+    end: "11:00",
+    filterId: 121,
+  },
+  {
+    id: 12345,
+    title: "Lógica",
+    theoretical: true,
+    shift: "TP2",
+    building: "7",
+    room: "0.02",
+    day: 2,
+    start: "09:00",
+    end: "11:00",
+    filterId: 124,
+  },
+];
 
-export default function Schedule() {
+export default function Schedule({ filters }) {
   const [events, setEvents] = useState([]);
+  const [selectedFilters, setSelectedFilters] = useState([]);
 
-  const formatEvents = () => {
-    const mock = [
-      {
-        id: 12345,
-        title: "Análise",
-        theoretical: true,
-        shift: "TP2",
-        building: "7",
-        room: "0.02",
-        day: 1,
-        start: "09:00",
-        end: "11:00",
-      },
-      {
-        id: 12345,
-        title: "Lógica",
-        theoretical: true,
-        shift: "TP2",
-        building: "7",
-        room: "0.02",
-        day: 2,
-        start: "09:00",
-        end: "11:00",
-      },
-    ];
+  const formats = useMemo(
+    () => ({
+      dayFormat: (date, _, localizer) => localizer.format(date, "ddd"),
+    }),
+    []
+  );
 
-    const formatedEvents = mock.map((mockEvent) => {
+  const formatEvents = useCallback(() => {
+    const myMock = mock.filter((event) =>
+      selectedFilters.includes(event.filterId)
+    );
+
+    const formatedEvents = myMock.map((mockEvent) => {
       const [startHour, startMinute] = mockEvent.start.split(":");
       const [endHour, endMinute] = mockEvent.end.split(":");
 
       return {
         ...mockEvent,
+        title: `${mockEvent.title} ${mockEvent.shift} - Edf. ${mockEvent.building} Sala ${mockEvent.room}`,
         start: new Date(
           moment().day(mockEvent.day).hour(startHour).minute(startMinute)
         ),
@@ -59,11 +72,11 @@ export default function Schedule() {
     });
 
     setEvents(formatedEvents);
-  };
+  }, [selectedFilters]);
 
   useEffect(() => {
     formatEvents();
-  }, []);
+  }, [selectedFilters, formatEvents]);
 
   return (
     <Layout>
@@ -77,6 +90,7 @@ export default function Schedule() {
         <Calendar
           toolbar={false}
           localizer={localizer}
+          formats={formats}
           events={events}
           startAccessor="start"
           endAccessor="end"
@@ -84,18 +98,30 @@ export default function Schedule() {
           defaultDate={new Date()}
           min={new Date("08:00 2022/01/01")}
           max={new Date("21:00 2022/01/01")}
-          //Custom event colors by their year (groupID)
-          eventPropGetter={(event) => {
-            const style = {
-              backgroundColor: colors[event.title],
-              border: "none",
-            };
-
-            return { style };
-          }}
           style={{ height: "90vh" }}
+        />
+      </div>
+
+      <div>
+        <CheckBox
+          filters={filters}
+          handleFilters={(myFilters) => setSelectedFilters(myFilters)}
         />
       </div>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const filterFilePath = path.join(process.cwd(), "data/filters.json");
+
+  const filterData = await fsPromises.readFile(filterFilePath);
+
+  const filters = JSON.parse(filterData);
+
+  return {
+    props: {
+      filters,
+    },
+  };
 }
