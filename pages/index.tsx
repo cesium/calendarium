75f195554ae965b87ed708df96e53a43db932f31
Rui Lopes <rui.lilokas2003@hotmail.com>
2022-09-17 14:49:47 +0100

    Refactor code (#34)
    * Refactor code
    
    * Run formatter

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 
 import Layout from "../components/Layout";
 import CheckBox from "../components/CheckBox";
-import BasicModal from "../components/Modal";
+import EventModal from "../components/Modal";
 
 const localizer = momentLocalizer(moment);
 
@@ -19,13 +19,14 @@ export default function Home({ events, filters }) {
   const configureDates = (event) => {
     event.start = new Date(event.start);
     event.end = new Date(event.end);
+
     return event;
   };
 
   const [Events, setEvents] = useState(events.map(configureDates));
   const [Filters, setFilters] = useState(filters);
-  const [selected, setSelected] = useState();
-  const [open, setOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState([]);
+  const [inspectEvent, setInspectEvent] = useState(false);
 
   const showNewEvents = (f) => {
     const filters = Object.values(f);
@@ -38,18 +39,26 @@ export default function Home({ events, filters }) {
     setEvents(newEvents);
   };
 
-  //Function to update the Filters state
   const handleFilters = (myFilters) => {
     const newFilters = { ...myFilters };
     setFilters(newFilters);
     showNewEvents(newFilters);
   };
 
-  // Function to update a selected event
-  const handleSelected = (mySelected) => {
-    setSelected(mySelected);
-    setOpen(!open);
+  const handleSelection = (event) => {
+    setSelectedEvent(event);
+    setInspectEvent(!inspectEvent);
   };
+
+  const colors = [
+    "#f07c54",
+    "#f0c954",
+    "#7b54f0",
+    "#f0547b",
+    "#5ac77b",
+    "#5532a8",
+    "#b70a0a",
+  ];
 
   return (
     <Layout isHome>
@@ -58,46 +67,37 @@ export default function Home({ events, filters }) {
           <Head>
             <title>Home | Calendarium</title>
             <meta name="Calendarium" content="Calendar of events and exams" />
-            <link rel="icon" href="/calendar-icon.ico" />~
+            <link rel="icon" href="/calendar-icon.ico" />
           </Head>
 
           <div id="APP">
             <Calendar
               localizer={localizer}
-              selected={selected}
-              onSelectEvent={handleSelected}
-              //Establishing some default definitions
+              selected={selectedEvent}
+              onSelectEvent={(event) => handleSelection(event)}
               defaultDate={new Date()}
               defaultView="month"
-              //Custom event colors by their year (groupID)
               eventPropGetter={(event) => {
-                var colors = [
-                  "#f07c54",
-                  "#f0c954",
-                  "#7b54f0",
-                  "#f0547b",
-                  "#5ac77b",
-                  "#5532a8",
-                  "#b70a0a",
-                ];
-
-                let newStyle = {
+                const newStyle = {
                   backgroundColor: colors[event.groupId],
                   border: "none",
                 };
 
                 return { style: newStyle };
               }}
-              //Using the array of all events
               events={Events}
-              //Limit the time for the events (Between 8:00 and 20:00)
               min={new Date(2022, 0, 1, 8, 0)}
               max={new Date(2022, 0, 1, 21, 0)}
               style={{ height: "90vh" }}
             />
           </div>
 
-          {open && <BasicModal content={selected} />}
+          {inspectEvent && (
+            <EventModal
+              selectedEvent={selectedEvent}
+              setInspectEvent={setInspectEvent}
+            />
+          )}
 
           <div className={styles.filter}>
             <CheckBox
