d894add40ced0a534783fd11e63267c84cc1c0f9
MarioRodrigues10 <mjsrodrigues4@gmail.com>
2022-09-18 15:57:18 +0100

    remove theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 import "../styles/globals.css";
-import { ThemeProvider } from "../components/Theme/Theme";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider>
       <Component {...pageProps} />
-    </ThemeProvider>
   );
 }
 
