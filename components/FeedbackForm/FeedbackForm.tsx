f33b4aa335866e1a9726f9b967bb712067dbe663
Diogo Matos <diogomatosemail@gmail.com>
2022-09-25 15:31:04 +0100

    Navbar and Footer style tweaks

--- components/FeedbackForm/FeedbackForm.tsx
+++ components/FeedbackForm/FeedbackForm.tsx
@@ -4,7 +4,9 @@ import styles from "./feedbackform.module.scss";
 const FeedbackForm = () => {
   return (
     <div className={styles.footer}>
-      <p className={styles.title}>Anything wrong or missing?</p>
+      <p className={styles.title}>
+        Anything wrong or missing? <i className="bi bi-bug-fill"></i>
+      </p>
       <Link href="https://forms.gle/C2uxuUKqoeqMWfcZ6">
         Contact us and give your feedback
       </Link>
