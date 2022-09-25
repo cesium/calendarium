03f4eaea5231e9c610d5a7035317f624f3ba2d8e
Diogo Matos <diogomatosemail@gmail.com>
2022-09-25 23:03:57 +0100

    Formatting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 function Calendarium({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
