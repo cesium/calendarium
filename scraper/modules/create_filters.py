395d0955cdba20dfe2ea6ac99f6935d0b1e3192e
Dário Guimarães <49988070+Darguima@users.noreply.github.com>
2023-03-08 17:55:38 +0000

    Schedule Scraper (#90)

---
+++ scraper/modules/create_filters.py
@@ -0,0 +1,126 @@
+from re import split
+
+
+def create_filters(shifts: list[dict], subjects: list[dict]):
+    """
+    Generate the filters.json to Calendarium.
+
+    Parameters
+    ----------
+    shifts : list[dict]
+      The shifts list.
+
+      [{
+        "id": int,
+
+        "title": str,
+
+        "theoretical": bool,
+
+        "shift": string,
+
+        "building": string,
+        "room": string,
+
+        "day": int,
+
+        "start": string,
+        "end": string,
+
+        "filterId": int
+      }]
+
+    subjects : list[dict]
+      The subjects list.
+
+      [{
+        "id": int, # filterId
+        "subjectId": int, 
+        "name": string,
+        "short_name": string,
+        "year": int,
+        "semester": int
+      }]
+
+    Returns
+    -------
+    [{
+      "id": int,
+      "name": str,
+      "groupId": int,
+      "semester": int,
+      "shifts": [str]
+    }]
+    """
+
+    filters = []
+
+    for subject in subjects:
+        subject_shifts = []
+
+        for shift in shifts:
+            if shift["filterId"] == subject["id"] and shift["shift"] not in subject_shifts:
+                subject_shifts.append(shift["shift"])
+
+        # Ordering shifts by number
+        subject_shifts = sorted(
+            subject_shifts, key=lambda shift: int(split('(\d+)', shift)[1]))
+        # Ordering shifts by letters
+        subject_shifts = sorted(
+            subject_shifts, key=lambda shift: split('(\d+)', shift)[0])
+        # Ordering shifts by type (theoretical first)
+        theoretical_shifts = [
+            shift for shift in subject_shifts if shift[0] == "T" and shift[1].isnumeric()]
+        subject_shifts = [
+            shift for shift in subject_shifts if shift not in theoretical_shifts]
+
+        subject_shifts = theoretical_shifts + subject_shifts
+
+        filters.append({
+            "id": subject["id"],
+            "name": subject["short_name"],
+            "groupId": subject["year"],
+            "semester": subject["semester"],
+            "shifts": subject_shifts
+        })
+
+    filters = [
+        {
+            "id": 0,
+            "name": "CeSIUM",
+            "groupId": 0,
+            "semester": 0
+        },
+        {
+            "id": 1,
+            "name": "UMinho",
+            "groupId": 0,
+            "semester": 0
+        },
+        {
+            "id": 2,
+            "name": "SEI",
+            "groupId": 0,
+            "semester": 0
+        },
+        {
+            "id": 3,
+            "name": "CoderDojo",
+            "groupId": 0,
+            "semester": 0
+        },
+        {
+            "id": 4,
+            "name": "JOIN",
+            "groupId": 0,
+            "semester": 0
+        },
+        {
+            "id": 5,
+            "name": "JORDI",
+            "groupId": 0,
+            "semester": 0
+        }
+    ] + filters
+
+    return filters
---
+++ scraper/modules/schedule_scraper.py
@@ -0,0 +1,130 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+
+from modules.increment_time import increment_time, Time
+
+
+def schedule_scraper(driver: WebDriver, subject_codes: list[dict[str, int]]):
+    """
+    Scrape the UM schedule of a given driver.
+    Warning: the schedule need be already on the page.
+
+    Parameters
+    ----------
+    driver : WebDriver
+      The selenium driver. Need have the schedule ready
+
+    subject_codes : list[dict[str, int]]
+      Every subject has its subject ID and filter ID. This IDs are stored on a list of dicts with the format:
+
+      [{
+        "id": int,
+        "filterId": int
+      }]
+
+    Returns
+    -------
+    [{
+      "id": int,
+
+      "title": str,
+
+      "theoretical": bool,
+
+      "shift": string,
+
+      "building": string,
+      "room": string,
+
+      "day": int,
+
+      "start": string,
+      "end": string,
+
+      "filterId": int
+    }]
+    """
+
+    classes = []
+
+    cell_height = driver.find_element(
+        By.CSS_SELECTOR, '.rsContentTable tr').size["height"]
+
+    first_hour_on_schedule = driver.find_element(
+        By.CSS_SELECTOR, '.rsVerticalHeaderTable tbody tr th div').text.split(':')
+    first_time: Time = {
+        "hour": int(first_hour_on_schedule[0]),
+        "minute": int(first_hour_on_schedule[1])
+    }
+
+    extracted_rows = driver.find_elements(
+        By.CSS_SELECTOR, '.rsContentTable tr')
+
+    for row_index, row in enumerate(extracted_rows):
+        # elapsed time in minutes = index * 30 -> each row is a 30 min block
+        start_time = increment_time(first_time, row_index * 30)
+
+        extracted_columns = row.find_elements(By.CSS_SELECTOR, 'td')
+
+        for column_index, column in enumerate(extracted_columns):
+            weekday = column_index
+
+            extracted_classes = column.find_elements(By.CSS_SELECTOR, '.rsApt')
+
+            for class_container in extracted_classes:
+                class_info = class_container.find_elements(
+                    By.CSS_SELECTOR, '.rsAptContent')
+
+                if len(class_info) == 0:
+                    continue
+
+                subject, location, shift = class_info[0].text.split('\n')
+
+                if subject.lower() in subject_codes.keys():
+                    subject_ids = subject_codes[subject.lower()]
+                else:
+                    print(
+                        f"\t\033[93m\033[1mWARNING:\033[0m {subject} isn't present on scraper/subjects.json. Using default values")
+                    subject_ids = {
+                        "id": 0,
+                        "filterId": 0
+                    }
+
+                duration_in_minutes = (
+                    (class_container.size["height"] + 4) / cell_height) * 30
+                end_time = increment_time(start_time, duration_in_minutes)
+
+                shift_type = "".join(
+                    [char for char in shift if char.isalpha()])
+
+                _, build, room = location.removeprefix(
+                    "[").removesuffix("]").split(" - ")
+                build_number = build.split(' ')[1]
+
+                if int(build_number) <= 3:
+                    build_number = "CP" + build_number
+
+                start_time_string = f"{start_time['hour']:02}:{start_time['minute']:02}"
+                end_time_string = f"{end_time['hour']:02}:{end_time['minute']:02}"
+
+                classes.append({
+                    "id": subject_ids["id"],
+
+                    "title": subject,
+
+                    "theoretical": shift_type.upper() == "T",
+
+                    "shift": shift,
+
+                    "building": build_number,
+                    "room": f"{room}",
+
+                    "day": weekday,
+
+                    "start": start_time_string,
+                    "end": end_time_string,
+
+                    "filterId": subject_ids["filterId"]
+                })
+
+    return classes
