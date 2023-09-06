e92c778cb9bb4ebb7d271fbf6419d048d65b62ef
Diogo Matos <diogomatosemail@gmail.com>
2023-09-06 16:35:09 +0100

    debug api netlify error

--- pages/api/export/events.tsx
+++ pages/api/export/events.tsx
@@ -38,15 +38,24 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     "attachment; filename=calendarium-events.ics"
   );
 
+  console.log("API request received: /api/export/events\n");
+  console.log("Content-Type and Content-Disposition SET\n");
+
   // Fetch filter data from JSON
   const filters: IFilterDTO[] = JSON.parse(
     fs.readFileSync("data/filters.json", "utf-8")
   );
+  console.log("Filters fetched from JSON\n");
+  console.log("Filters: ", filters, "\n");
   // Fetch filter names
   const filtersNames: string[] = filters.map((f) => f.name);
+  console.log("FiltersNames fetched\n");
+  console.log("FiltersNames: ", filtersNames, "\n");
 
   // Fetch query keys
   const queryKeys: string[] = Object.keys(req.query);
+  console.log("QueryKeys fetched\n");
+  console.log("QueryKeys: ", queryKeys, "\n");
 
   // Check if the API request is valid
   const valid: boolean =
@@ -54,10 +63,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     queryKeys.map((key) => filtersNames.includes(key)).every((v) => v === true);
 
   if (valid) {
+    console.log("API request is valid\n");
     // Fetch event data from JSON
     const eventsData: IEventDTO[] = JSON.parse(
       fs.readFileSync("data/events.json", "utf-8")
     );
+    console.log("Events fetched from JSON\n");
+    console.log("Events: ", eventsData, "\n");
 
     // Converts the start and end date strings of an event into Date objects
     const configureDates = (event) => {
@@ -68,11 +80,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     // Convert date strings into Date objects, for all events
     const events: IFormatedEvent[] = eventsData.map(configureDates);
+    console.log("Events converted to Date objects\n");
 
     // Convert query keys (filter names) into filter ids
     const checked: number[] = queryKeys.map(
       (key) => filters.find((f) => f.name === key).id
     );
+    console.log("QueryKeys converted to filter ids\n");
+    console.log("Checked: ", checked, "\n");
 
     // Filter events based on the query params
     const filteredEvents: IFormatedEvent[] = events.filter(
@@ -80,9 +95,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         return checked.includes(event.filterId) || event.filterId === -1;
       }
     );
+    console.log("Events filtered\n");
+    console.log("FilteredEvents: ", filteredEvents, "\n");
 
     // Convert events to ICS format
     const icsEvents: EventAttributes[] = convertEventsToICS(filteredEvents);
+    console.log("Events converted to ICS format\n");
+    console.log("ICS Events: ", icsEvents, "\n");
 
     // Create ICS file and return it
     createEvents(icsEvents, (error, value) => {
@@ -92,7 +111,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         return;
       }
 
+      console.log("ICS file created\n");
+      console.log("ICS file: ", value, "\n");
       res.status(200).send(value);
     });
-  } else res.status(400).send("Invalid request\n");
+  } else {
+    console.log("API request is invalid\n");
+    res.status(400).send("Invalid request\n");
+  }
 };
