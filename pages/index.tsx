eb3e6df31718340ee0932e8b8a1ef9df4a3d3dd5
chico ferreira <36338391+chicoferreira@users.noreply.github.com>
2023-09-10 15:29:20 +0100

    Change schedule time gutter from 12h to 24h format (#130)
    * Change schedule time gutter from 12h to 24h format
    
    * Change time gutter format in events
    
    * Change start day from Sunday to Monday in events

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,12 @@ export interface IFormatedEvent {
   filterId: number;
 }
 
+moment.locale("en", {
+  week: {
+    dow: 1,
+  },
+});
+
 const localizer = momentLocalizer(moment);
 
 export default function Home({ events, filters }) {
@@ -160,7 +166,7 @@ export default function Home({ events, filters }) {
       return "";
     },
     timeGutterFormat: (date, culture, localizer) =>
-      localizer.format(date, "h A", culture).replace(/^0+/, ""),
+      localizer.format(date, "HH\\h", culture).replace(/^0+/, ""),
   };
 
   const minDate = new Date();
