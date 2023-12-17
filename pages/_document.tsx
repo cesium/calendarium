1b7d6a35a31b3dfd3b65ba920dad659c386f1827
Diogo Matos <diogomatosemail@gmail.com>
2023-12-17 01:56:01 +0000

    fix: missing meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel="manifest" href="/manifest.webmanifest" />
         <link rel="apple-touch-icon" href="/icon-512x512.png"></link>
         {/* Splash Screen configuration for IOS devices */}
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
         <link
           href="/pwa/splashscreens/iphone5_splash.png"
           media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
