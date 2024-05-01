5edaf21e8a08e6ff26d13334dbe6cb6ea41051dc
Diogo Matos <diogomatosemail@gmail.com>
2024-05-01 15:11:58 +0100

    feat: improve today button in toolbar for better understanding (#210)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,21 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { useTheme } from "next-themes";
-import { useEffect } from "react";
 
 export default function Document() {
   return (
     <Html lang="en" className="dark">
       <Head>
+        {/* Fonts and Icons */}
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
+        {/* Web App Related */}
         <link rel="manifest" href="/manifest.webmanifest" />
         <link rel="apple-touch-icon" href="/pwa/ios/512.png"></link>
         {/* Splash Screen configuration for IOS devices */}
